414452abd9676640db7abf2c0c4a83a07a9b5b1b
AndreyMoraesFriedel <andreymoraesfriedel@gmail.com>
2023-12-03 12:22:03 -0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -12,36 +12,28 @@ client.on('ready', () => {
 });
 
 client.on('message', message => {
-    if (message.body.toLocaleLowerCase() === 'oi') {
+    // Verificar se a mensagem é de um grupo
+    if (message.author || message.fromMe || message.isForwarded || message.broadcast) {
+        // Ignorar mensagens que são de grupos ou encaminhadas
+        return;
+    }
+
+    //variavel para obter a mensagem enviada
+    const str = message.body;  
+
+    //Possíveis saudações
+    if (/tudo bem?/i.test(str) || /bom dia/i.test(str) || /boa tarde/i.test(str) || /boa noite/i.test(str)) {  
         client.sendMessage(message.from, 'Seja Bem Vindo! Meu nome é *Fabi*, sua assistente virtual da Lojas Fabricia. 🙋‍♀️');
         client.sendMessage(message.from, 'Como posso te ajudar? Digite o *numero* da opção🌟\n\n1 - Endereço da Loja\n2 - Crediário\n3 - Horário De Funcionamento\n4 - Possui Estacionamento?\n5 - Falar Conosco');
     }
-   
-    if (message.body.toLocaleLowerCase() === 'bom dia') {
-        client.sendMessage(message.from, 'Seja Bem Vindo! Meu nome é *Fabi* sua assistente virtual da Lojas Fabricia. 🙋‍♀️');
-        client.sendMessage(message.from, 'Como posso te ajudar? Digite o *numero* da opção🌟\n\n1 - Endereço da Loja\n2 - Crediário\n3 - Horário De Funcionamento\n4 - Possui Estacionamento?\n5 - Falar Conosco');
-    }
-   
-    if (message.body.toLocaleLowerCase() === 'boa tarde') {
-        client.sendMessage(message.from, 'Seja Bem Vindo! Meu nome é *Fabi* sua assistente virtual da Lojas Fabricia. 🙋‍♀️');
-        client.sendMessage(message.from, 'Como posso te ajudar? Digite o *numero* da opção🌟\n\n1 - Endereço da Loja\n2 - Crediário\n3 - Horário De Funcionamento\n4 - Possui Estacionamento?\n5 - Falar Conosco');
-    }
-   
-    if (message.body.toLocaleLowerCase() === 'boa noite') {
-        client.sendMessage(message.from, 'Seja Bem Vindo! Meu nome é *Fabi*, sua assistente virtual da Lojas Fabricia. 🙋‍♀️');
-        client.sendMessage(message.from, 'Como posso te ajudar? Digite o *numero* da opção🌟\n\n1 - Endereço da Loja\n2 - Crediário\n3 - Horário De Funcionamento\n4 - Possui Estacionamento?\n5 - Falar Conosco');
-    }
-   
-    if (message.body.toLocaleLowerCase() === 'ola') {
-        client.sendMessage(message.from, 'Seja Bem Vindo! Meu nome é *Fabi*, sua assistente virtual da Lojas Fabricia. 🙋‍♀️');
-        client.sendMessage(message.from, 'Como posso te ajudar? Digite o *numero* da opção🌟\n\n1 - Endereço da Loja\n2 - Crediário\n3 - Horário De Funcionamento\n4 - Possui Estacionamento?\n5 - Falar Conosco');
-    }
-   
-    if (message.body.toLocaleLowerCase() === '1') {
+
+    //Localização da Loja
+    if (str == '1' || /onde fica/i.test(str) || /local/i.test(str) || /localização/i.test(str)) {
         client.sendMessage(message.from, 'Loja Fabrícia fica em Blumenau - SC, Rua 2 de Setembro n 3649. 🏬\n\nAo lado da antiga empresa de ônibus da Glória defronte ao Banco do Brasil da Itoupava Norte. 🏦\n\nAo lado da lojinha Portal BAZAR de $ 10 20 ... 😘');
     }
-   
-    if (message.body.toLocaleLowerCase() === '2') {
+
+    //Crediário da Loja
+    if (str == '2' || /fazer crediário/i.test(str) || /fazer crediario/i.test(str) || /faço crediário/i.test(str) || /faço crediario/i.test(str)) {
         client.sendMessage(message.from, 'Caso ainda não seja cadastrada(o) junto ao sistema de crediário *CREDILOJA*, basta enviar fotos bem legíveis do seu CPF, RG, comprovantes de renda e residência. 😊');
         client.sendMessage(message.from, 'Caso seja *autônoma(o)*, apenas informe a profissão e a data desde quando está nela.');
         client.sendMessage(message.from, 'Caso seja *casada(o)*, também apenas preciso que me passe o nome completo do marido/esposa.');
@@ -49,19 +41,20 @@ client.on('message', message => {
         client.sendMessage(message.from, '*Caso tenha restrição junto ao SPC, a venda no crediário fica bloqueada, até que consiga solucionar esta situação...*');
     }
    
-    if (message.body.toLocaleLowerCase() === '3') {
+    //Horario da Loja
+    if (str == '3' || /horas/i.test(str) || /horário/i.test(str) || /horario/i.test(str)) {
         client.sendMessage(message.from, 'Horário de Funcionamento:\n\nSegunda a Sexta das 8:30 às 18:00 hrs\nSábados das 8:30 às 17:00 hrs\n\n*Todos os dias sem fechar para o almoço.* 🕒🛍️');
         client.sendMessage(message.from, 'Venha apenas nos fazer uma visitinha! Já estamos te esperando 🤗😘');
     }
 
-
-    if (message.body.toLocaleLowerCase() === '4') {
+    //Estacionamento da Loja
+    if (str == '4' || /estacionamento/i.test(str)) {
         client.sendMessage(message.from, '*Sim  !!!* Pode estacionar em qualquer vaga defronte ao prédio, pois as mesmas são públicas.🤗👍');
     }
 
-
-    if (message.body.toLocaleLowerCase() === '5') {
-        client.sendMessage(message.from, 'Ola amiga(o), responderemos sua mensagem assim que possível.🤗👍');
+    //Opção 'Falar Conosco'
+    if (str == '5') {
+        client.sendMessage(message.from, 'Olá amiga(o), responderemos sua mensagem assim que possível.🤗👍');
     }
 }); 
 
